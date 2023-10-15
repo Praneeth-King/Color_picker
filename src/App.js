d1beff0c64d40874cabda5b5d3860748b1597852
Praneeth-King <praneeth.donkana2003@gmail.com>
2023-10-15 23:00:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './index.css';
 
-const App= () => {
-  const [result, setResult] = useState("");
-  const handleClick = (e) => {
-    setResult(result.concat(e.target.name));
-  }
-  const clear =() => {
-    setResult("");
-  }
-  const calculate = ()=>{
-    try{
-      setResult(eval(result).toString());
-    } catch(err){
-      setResult('Error');
-    }
-    
-  }
+function App() {
+  const colors = [
+    '#ff0000', '#00FF01', '#0000FE', '#FFFF00', '#FF00FE', '#00FFFF', '#FEA500', '#81007F', '#FFC0CB',
+    '#008001', '#FE6347', '#01CED1', '#894516', '#FF8B00', '#4682B4', '#FED700'
+  ];
+
+  const [colorBoxesVisible, setColorBoxesVisible] = useState(false);
+  const [buttonBackgroundColor, setButtonBackgroundColor] = useState('rgb(70, 67, 67)');
+
+  const handleButtonClick = () => {
+    setColorBoxesVisible(!colorBoxesVisible);
+  };
+
+  const handleColorBoxClick = (color) => {
+    setButtonBackgroundColor(color);
+    setColorBoxesVisible(false);
+  };
+
   return (
-    <>
-      <div className="container">
-        <section className="screen">
-          {result}
-        </section>
-        <section className="button-container">
-            <div className="column">
-              <button name="7" onClick={handleClick}>7</button>
-              <button name="4" onClick={handleClick}>4</button>
-              <button name="1" onClick={handleClick}>1</button>
-              <button name="0" onClick={handleClick}>0</button>
-            </div>
-            <div className="column">
-              <button name="8" onClick={handleClick}>8</button>
-              <button name="5" onClick={handleClick}>5</button>
-              <button name="2" onClick={handleClick}>2</button>
-              <button name="." onClick={handleClick}>.</button>
-            </div>
-            <div className="column">
-              <button name="9" onClick={handleClick}>9</button>
-              <button name="6" onClick={handleClick}>6</button>
-              <button name="3" onClick={handleClick}>3</button>
-              <button name="=" onClick={calculate}  id="result">=</button>
-            </div>
-            <div className="column-special">
-              <button onClick={clear} id="clear">DEL</button>
-              <button name="/" onClick={handleClick}>&divide;</button>
-              <button name="*" onClick={handleClick}>&times;</button>
-              <button name="-" onClick={handleClick}>&minus;</button>
-              <button name="+" onClick={handleClick}>+</button>
-            </div>
-        </section>
+    <div id='container'>
+      <h1>Color Picker</h1>
+      <div id='colors'>
+        {colorBoxesVisible &&
+          colors.map((color, index) => (
+            <div
+              key={index}
+              className='color-box'
+              style={{ backgroundColor: color }}
+              onClick={() => handleColorBoxClick(color)}
+            ></div>
+          ))}
+        <div className="break">
+          <button onClick={handleButtonClick}>Pick a color</button>
+        </div>
       </div>
-    </>
+      <style>
+        {`
+          #container #colors button {
+            border: none;
+            color: white;
+            background: ${buttonBackgroundColor};
+            font-size: 20px;
+            width: 150px;
+            height: 50px;
+            cursor: pointer;
+          }
+        `}
+      </style>
+    </div>
   );
 }
 
